81a7f9084c458a09524d3b2c02959d2fd43c96fb
Eugene Avdeev <ea.avdeev@gmail.com>
2023-08-26 23:46:06 +0300

    ci(root/model): create 'movie' model

---
+++ models/movie.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+// const { isURL } = require('../utils/utils');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v), // join('')
+      message: 'не является валидной URL ссылкой!',
+    },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'не является валидной URL ссылкой!',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'не является валидной URL ссылкой!',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId, // сохр идентифик. фильма
+    ref: 'user', // ссылка на модель пользователя
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+    default: 'Film name',
+  },
+  nameEn: {
+    type: String,
+    required: true,
+    default: 'Film name',
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('movie', movieSchema); // создаём модель и экспортируем
