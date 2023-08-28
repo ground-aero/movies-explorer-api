e14a9cc6721fb4155a3886038d856a7f1744573a
Eugene Avdeev <ea.avdeev@gmail.com>
2023-08-29 02:50:27 +0300

    fix(routes/): router.delete movieId

--- routes/movies.js
+++ routes/movies.js
@@ -8,6 +8,6 @@ const auth = require('../middlewares/auth');
 
 router.post('/movies', auth, createMovie); // создаёт польз-ля с переданными в теле name, email, pass
 router.get('/movies', auth, getMovies);
-router.delete('/movies/movieId', auth, deleteMovieId);
+router.delete('/movies/:movieId', auth, deleteMovieId);
 
 module.exports = router;
