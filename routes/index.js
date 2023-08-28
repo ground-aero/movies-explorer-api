48686eef70a580324fca1761d66a470ae59a575a
Eugene Avdeev <ea.avdeev@gmail.com>
2023-08-28 15:04:46 +0300

    feat(routes/index, movies): implement movies

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const NotFoundError = require('../errors/not-found-err');
 // const { createUser, login } = require('../controllers/users');
 const authorization = require('./authorization');
 const users = require('./users');
+const movies = require('./movies');
 
 router.use(authorization);
 router.use(users);
+router.use(movies)
 
 router.use('/*', (req, res, next) => next(new NotFoundError('такой маршрут не найден'))); // 404
 
--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const {
   createMovie,
 } = require('../controllers/movies');
+const auth = require('../middlewares/auth');
 
-router.post('/movies', createMovie); // создаёт польз-ля с переданными в теле name, email, pass
+router.post('/movies', auth, createMovie); // создаёт польз-ля с переданными в теле name, email, pass
 
 module.exports = router;
