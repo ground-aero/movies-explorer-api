1023d61dc00bea85c37e2087121cbfdfe5dc4a4c
Eugene Avdeev <ea.avdeev@gmail.com>
2023-08-29 20:11:01 +0300

    comment:(middlewares) auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,5 @@
-/** Мидлвэр авторизации. Верифицирует токен из заголовков
- * Если с токеном всё в порядке, мидлвэр должен добавлять пейлоуд токена в объект запроса req.user
- * и вызывать next: */
+/** Мидлвэр будет вызываться на каждый запрос,
+ * проверять хедер определенных запросов на наличие авторизации, и вызывать next: */
 require('dotenv').config();
 const jsonwebtoken = require('jsonwebtoken');
 const AuthoErr = require('../errors/autho-err');
@@ -8,31 +7,33 @@ const { JWT_SECRET } = require('../config');
 
 const { NODE_ENV } = process.env;
 
-/** мидлвэр будет вызываться на каждый запрос.
- * проверять хедер определенных запросов на наличие авторизации */
 const auth = (req, res, next) => {
-// ToDo: check token valid, and go next. If (valid) {go next}, else error
-  /** взять заголовок authorization */
+  /** ToDo: 1. достанем токен из заголовка
+   *  взять заголовок authorization */
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new AuthoErr('Необходима авторизация *');
   }
 
-  /** достать jwt из authorization хедера, удалить Bearer из загол */
+  /** достать jwt из хедера authorization, удалить Bearer из загол */
   const token = authorization.replace('Bearer ', '');
   // const token = authorization.split('Bearer ')[1];
   let payload;
 
-  /** Верификация jwt по секретному ключу */
+  /** ToDo: 2. check token valid, and go next. If (valid) {go next}, else error
+   *  Верификация jwt по секретному ключу */
   try {
+    /** Токен верифицирован, пейлоуд извлечён.
+     * проверить что jwt валидный с помощью библ jsonwebtoken */
     payload = jsonwebtoken.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key2');
-    /** проверить что jwt валидный с помощью библ jsonwebtoken */
   } catch (error) {
     throw new AuthoErr('передан неверный логин или пароль -');
   }
 
-  req.user = payload; // добавить пейлоуд токена в объект запроса юзера !
-  return next();
+  /** ToDo: 3. wright down the payload into req object — свойство req.user
+   *  Так следующий мидлвэр сможет определить, кем этот запрос был выполнен */
+  req.user = payload;
+  return next(); // let the req go further
 };
 
 module.exports = auth;
